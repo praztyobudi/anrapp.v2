3cc9cc16800f0982f184e686358c1b7dedad56f4
root <root@praze-deb.net>
2025-04-09 15:43:19 +0700

    awal

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module anrbackend
+
+go 1.24.2
---
+++ backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Backend Golang jalan!")
+	})
+	fmt.Println("Server backend jalan di http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
